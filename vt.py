219d6cee81f85ae09ad2ad5ae396abc1481da9b1
Robert Svensson <robert@artandhacks.se>
2016-10-26 15:31:45 +0200

    Added sha256 report functionality

--- vt.py
+++ vt.py
@@ -11,7 +11,24 @@ class ConnectionHandle(object):
         return result
 
     def sendURLtoVirusTotal_returnJSON(self,url):
-        virusTotalURL = 'https://www.virustotal.com/vtapi/v2/url/scan'
-        payload = {'url': url, 'apikey': self.apiKey}
-        r = requests.post(virusTotalURL, params=payload)
-        return r.content
+        try:
+            virusTotalURL = 'https://www.virustotal.com/vtapi/v2/url/scan'
+            payload = {'url': url, 'apikey': self.apiKey}
+            r = requests.post(virusTotalURL, params=payload)
+            return r.content
+        except Exception as e:
+            print 'Something when very wrong: %s' % (e)
+
+    def getSHA256report(self,sha256):
+        result = self.sendSHA256sumToVirusTotal_returnJSON(sha256)
+        return result
+
+    def sendSHA256sumToVirusTotal_returnJSON(self,sha256):
+        try:
+            virusTotalURL = 'https://www.virustotal.com/vtapi/v2/file/report'
+            payload = {'resource':sha256,'apikey':self.apiKey}
+            r = requests.post(virusTotalURL,params = payload)
+            return r.content
+
+        except Exception as e:
+            print 'Something went wrong: %s' % (e)
